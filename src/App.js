d2a9e6ebf5c5af58d628fd687c4d46a2489bf4ea
Mahsan Ojaghian <77487326+mahsan-ojaghian@users.noreply.github.com>
2021-07-18 03:17:56 +0430

    app component updated.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import "./styles.css";
-
-export default function App() {
+import React from 'react';
+import Review from './Review';
+function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <main>
+      <section className='container'>
+        <div className='title'>
+          <h2>our reviews</h2>
+          <div className='underline'></div>
+        </div>
+        <Review />
+      </section>
+    </main>
   );
 }
+
+export default App;
